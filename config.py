c977f595cd1f6eae44584ec446e5a43b4f06dca8
OliMa <olimarmor01@gmail.com>
2022-06-22 19:50:34 +0100

    make it better

---
+++ config.py
@@ -0,0 +1,5 @@
+PREFIX = "?"
+#TOKEN = 'OTI1ODI2MDA5MTM4MzMxNzA5.YcywqQ.bIJQZ1K5hnNOw9SPcmlPqX0cNrg'
+TOKEN = 'OTI4NzYyNzIyNTU0ODkyMzM5.YddfsA.xsCDdjpIHU8OW9aHbL1DZ1m6Ybc'
+FIREURL = 'https://discord-bot-f3c69-default-rtdb.europe-west1.firebasedatabase.app/'
+FIREBASE = '/discord-bot-f3c69-default-rtdb/Tickets'
